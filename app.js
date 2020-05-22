b0f081db6fd387cd5a05e8fba129dd3291ea80f0
root <root@gladi.localdomain>
2020-05-22 16:40:57 +0200

    added nodemon

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +13,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 
 app.use(logger('dev'));
 app.use(express.json());
