9415880fa2f8e3584762e5f5035b5c6409cc2e87
root@wsl <root@wsl>
2020-05-26 21:05:14 +0200

    🐵

--- controllers/landing.js
+++ controllers/landing.js
@@ -2,13 +2,20 @@ const models = require('../models')
 
 exports.get_landing = function(req, res, next) {
     res.render('landing', { title: 'Project Stream Italia' });
-  }
+}
 
-  exports.submit_lead = function(req, res, next) {
-    console.log("lead email: ", req.body.lead_email)
-    return models.Lead.create({
-      email: req.body.lead_email
-    }).then(lead => {
-      res.redirect('/');
-    })
-  }
+exports.submit_lead = function(req, res, next) {
+  console.log("lead email: ", req.body.lead_email)
+  return models.Lead.create({
+    email: req.body.lead_email
+  }).then(lead => {
+    res.redirect('leads');
+  })
+}
+
+exports.show_leads = function(req, res, next) {
+  models.Lead.findAll().then(leads => {
+    res.render('landing', { title: 'Project Stream Italia', leads: leads });
+
+  })
+}
